2081ed515cbddee29d9824913d145f8fea10d593
Mahesh Mani <manimahesh@hotmail.com>
2018-05-08 22:37:31 -0700

    Initial Upload

--- MyMobileApp/src/com/borland/sample/Insurance.java
+++ MyMobileApp/src/com/borland/sample/Insurance.java
@@ -1,16 +1,9 @@
 package com.borland.sample;
-// This is a sample comment.
 
 import com.borland.silktest.jtf.MobileBaseState;
-
 import org.junit.Before;
 import org.junit.Test;
 import com.microfocus.silktest.jtf.mobile.MobileTextField;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import com.microfocus.silktest.jtf.mobile.MobileButton;
 import com.microfocus.silktest.jtf.mobile.MobileObject;
 import com.borland.silktest.jtf.Desktop;
@@ -21,31 +14,6 @@ public class Insurance {
 
 	@Before
 	public void baseState() {
-		
-		/*
-		
-		String configFilter = System.getProperty("silkTest.configurationFilter");
-		String configName = System.getProperty("silkTest.configurationName");
-		
-		System.out.println("This is the configFilter: " + configFilter);
-		System.out.println("This is the configuration Name: " + configName);
-		
-		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(configFilter);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		JSONObject jsonObject = (JSONObject) obj;
-		String deviceName=(String) jsonObject.get("deviceName");
-		
-		System.out.println("Found deviceName to be: " + deviceName);
-		
-		*/
-		
 		MobileBaseState baseState = new MobileBaseState();
 		baseState.execute(desktop);
 	}
